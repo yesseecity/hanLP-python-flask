23a1abb2ab0510bfd6566e1d065e9fb62a5aa4d3
Tid <tid@breaktime.com.tw>
2017-02-23 15:05:20 +0800

    Convert response text to traditional chinese

--- server/hanlp-server.py
+++ server/hanlp-server.py
@@ -10,31 +10,46 @@ from flask_restful.representations.json import output_json
 output_json.func_globals['settings'] = {'ensure_ascii': False, 'encoding': 'utf8'}
 
 
+# Parameters 
+innerConvertEnable = True
+
+
 app = Flask(__name__)
 api = Api(app)
 
 parser = reqparse.RequestParser()
 hanLPLibPath = '/root/tid_home/myProject/python/hanLP/hanlp-1.3.2/'
-# hanLPLibPath = '/root/localhome/python/hanLP/hanlp-1.3.2/'
+# hanLPLibPath = '/root/localhome/hanLP/hanlp-1.3.2/'
 javaClassPath = hanLPLibPath+'hanlp-1.3.2.jar'+':'+hanLPLibPath
 
 startJVM(getDefaultJVMPath(), '-Djava.class.path='+javaClassPath, '-Xms1g', '-Xmx1g')
 HanLP = JClass('com.hankcs.hanlp.HanLP')
 
 parser.add_argument('content', type=str, required=True, help='content無法解析, *必要欄位')
-parser.add_argument('num', type=int, required=False)
-parser.add_argument('mode', type=int, required=False)
-
+parser.add_argument('convertMode', type=str)
+parser.add_argument('num', type=int)
+parser.add_argument('mode', type=int)
 @api.representation('application/json; charset=utf-8')
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-    def post(self):
-        content = parser.parse_args()['content']
-        print content
 
-        return {'state': 'success'}
 
+
+def innerConvert(inputString, mode):
+    if innerConvertEnable:
+        if mode == '2tc':
+            return HanLP.convertToTraditionalChinese(inputString)
+        elif mode == '2sc':
+            return HanLP.convertToSimplifiedChinese(inputString)
+        elif mode == '2hk':
+            return HanLP.s2hk(inputString)
+        elif mode == '2tw':
+            return HanLP.s2tw(inputString)
+        else:
+            return HanLP.s2tw(inputString)
+    else:
+        return inputString
+
+
+# For router
 class segment(Resource):
     def post(self):
         parser.add_argument('method', type=str, required=False)
@@ -93,33 +108,36 @@ class tcSegment(Resource):
 class indexTokenizer(Resource):
     def post(self):
         content = parser.parse_args()['content']
+        convertMode = parser.parse_args()['convertMode']
         if len(content)>0:
             IndexTokenizer = JClass('com.hankcs.hanlp.tokenizer.IndexTokenizer')
             segments = []
-            for v in IndexTokenizer.segment(content):
-                segments.append(str(v))
+            for v in IndexTokenizer.segment(innerConvert(content, '2sc')):
+                segments.append(innerConvert(str(v), convertMode))
             return {'response': segments}
         else:
             return {'error': { 'content': '長度不得為零'}}
 class jpNameRecognition(Resource):
     def post(self):
         content = parser.parse_args()['content']
+        convertMode = parser.parse_args()['convertMode']
         if len(content)>0:
             jpNameSegment = HanLP.newSegment().enableJapaneseNameRecognize(True)
             segments = []
-            for v in jpNameSegment.seg(content):
-                segments.append(str(v))
+            for v in jpNameSegment.seg(innerConvert(content, '2sc')):
+                segments.append(innerConvert(str(v), convertMode))
             return {'response': segments}
         else:
             return {'error': { 'content': '長度不得為零'}}
 class translatedNameRecognition(Resource):
     def post(self):
         content = parser.parse_args()['content']
+        convertMode = parser.parse_args()['convertMode']
         if len(content)>0:
-            jpNameSegment = HanLP.newSegment().enableTranslatedNameRecognize(True)
+            tranNameSegment = HanLP.newSegment().enableTranslatedNameRecognize(True)
             segments = []
-            for v in jpNameSegment.seg(content):
-                segments.append(str(v))
+            for v in tranNameSegment.seg(innerConvert(content, '2sc')):
+                segments.append(innerConvert(str(v), convertMode))
             return {'response': segments}
         else:
             return {'error': { 'content': '長度不得為零'}}
@@ -127,40 +145,44 @@ class keyword(Resource):
     def post(self):
         parser.add_argument('num', type=int, required=True, help='num無法解析, keyword的數量num為int, *必要欄位')
         content = parser.parse_args()['content']
+        convertMode = parser.parse_args()['convertMode']
         num = parser.parse_args()['num']
         if len(content)>0:
             segments = []
-            for v in HanLP.extractKeyword(content, int(num)):
-                segments.append(str(v))
+            for v in HanLP.extractKeyword(innerConvert(content, '2sc'), int(num)):
+                segments.append(innerConvert(str(v), convertMode))
             return {'response': segments}
         else:
             return {'error': { 'content': '長度不得為零'}}
 class nlpTokenizer(Resource):
     def post(self):
         content = parser.parse_args()['content']
+        convertMode = parser.parse_args()['convertMode']
         if len(content)>0:
             NLPTokenizer = JClass('com.hankcs.hanlp.tokenizer.NLPTokenizer')
             segments = []
-            for v in NLPTokenizer.segment(content):
-                segments.append(str(v))
+            for v in NLPTokenizer.segment(innerConvert(content, '2sc')):
+                segments.append(innerConvert(str(v), convertMode))
             return {'response': segments}
         else:
             return {'error': { 'content': '長度不得為零'}}
 class urlTokenizer(Resource):
     def post(self):
         content = parser.parse_args()['content']
+        convertMode = parser.parse_args()['convertMode']
         num = parser.parse_args()['num']
         if len(content)>0:
             URLTokenizer = JClass('com.hankcs.hanlp.tokenizer.URLTokenizer')
             sentence = []
-            for v in URLTokenizer.segment(content):
-                sentence.append(str(v))
+            for v in URLTokenizer.segment(innerConvert(content, '2sc')):
+                sentence.append(innerConvert(str(v), convertMode))
             return {'response': sentence}
         else:
             return {'error': { 'content': '長度不得為零'}}
 class notionalTokenizer(Resource):
     def post(self):
         content = parser.parse_args()['content']
+        convertMode = parser.parse_args()['convertMode']
         if len(content)>0:
             NotionalTokenizer = JClass('com.hankcs.hanlp.tokenizer.NotionalTokenizer')
             
@@ -170,68 +192,69 @@ class notionalTokenizer(Resource):
             #去除停用词+斷句
             # print NotionalTokenizer.seg2sentence(content)
 
-            for v in NotionalTokenizer.segment(content):
-            # for v in NotionalTokenizer.seg2sentence(content):
-                segments.append(str(v))
+            for v in NotionalTokenizer.segment(innerConvert(content, '2sc')):
+                segments.append(innerConvert(str(v), convertMode))
             return {'response': segments}
         else:
             return {'error': { 'content': '長度不得為零'}}
 class numberAndQuantifierRecognition(Resource):
     def post(self):
         content = parser.parse_args()['content']
+        convertMode = parser.parse_args()['convertMode']
         if len(content)>0:
             StandardTokenizer = JClass('com.hankcs.hanlp.tokenizer.StandardTokenizer')
             StandardTokenizer.SEGMENT.enableNumberQuantifierRecognize(True)
             segments = []
-            for v in StandardTokenizer.segment(content):
-                segments.append(str(v))
+            for v in StandardTokenizer.segment(innerConvert(content, '2sc')):
+                segments.append(innerConvert(str(v), convertMode))
             return {'response': segments}
         else:
             return {'error': { 'content': '長度不得為零'}}
 class organizationRecognition(Resource):
     def post(self):
         content = parser.parse_args()['content']
+        convertMode = parser.parse_args()['convertMode']
         if len(content)>0:
             segemntTool = HanLP.newSegment().enableOrganizationRecognize(True)
             segments = []
-            for v in segemntTool.seg(content):
-                segments.append(str(v))
+            for v in segemntTool.seg(innerConvert(content, '2sc')):
+                segments.append(innerConvert(str(v), convertMode))
             return {'response': segments}
         else:
             return {'error': { 'content': '長度不得為零'}}
 class phraseExtractor(Resource):
     def post(self):
-        # parser.add_argument('num', type=int, required=True, help='num無法解析, phrase的數量num為int, *必要欄位')
         content = parser.parse_args()['content']
+        convertMode = parser.parse_args()['convertMode']
         num = parser.parse_args()['num']
         if len(content)>0:
             segments = []
-            for v in HanLP.extractPhrase(content, num):
-                segments.append(str(v))
+            for v in HanLP.extractPhrase(innerConvert(content, '2sc'), num):
+                segments.append(innerConvert(str(v), convertMode))
             return {'response': segments}
         else:
             return {'error': { 'content': '長度不得為零'}}
 class placeRecognition(Resource):
     def post(self):
         content = parser.parse_args()['content']
+        convertMode = parser.parse_args()['convertMode']
         if len(content)>0:
             segemntTool = HanLP.newSegment().enablePlaceRecognize(True)
             segments = []
-            for v in segemntTool.seg(content):
-                segments.append(str(v))
+            for v in segemntTool.seg(innerConvert(content, '2sc')):
+                segments.append(innerConvert(str(v), convertMode))
             return {'response': segments}
         else:
             return {'error': { 'content': '長度不得為零'}}
 class posTagging(Resource):
     def post(self):
         content = parser.parse_args()['content']
-
-        segemntTool = HanLP.newSegment().enablePartOfSpeechTagging(True)
-
+        convertMode = parser.parse_args()['convertMode']
         if len(content)>0:
+            segemntTool = HanLP.newSegment().enablePartOfSpeechTagging(True)
             segments = []
-            for v in segemntTool.seg(content):
-                segments.append(str(v))
+            for v in segemntTool.seg(innerConvert(content, '2sc')):
+                segments.append(innerConvert(str(v), convertMode))
             return {'response': segments}
         else:
             return {'error': { 'content': '長度不得為零'}}
@@ -239,9 +262,11 @@ class posTagging(Resource):
 class rewrite(Resource):
     def post(self):
         content = parser.parse_args()['content']
+        convertMode = parser.parse_args()['convertMode']
         if len(content)>0:
             CoreSynonymDictionary = JClass('com.hankcs.hanlp.dictionary.CoreSynonymDictionary')
-            return {'response': CoreSynonymDictionary.rewrite(content)}
+            result = CoreSynonymDictionary.rewrite(innerConvert(content, '2sc'))
+            return {'response': innerConvert(result, convertMode)}
         else:
             return {'error': { 'content': '長度不得為零'}}
 
@@ -257,11 +282,12 @@ class rewrite(Resource):
 class summary(Resource):
     def post(self):
         content = parser.parse_args()['content']
+        convertMode = parser.parse_args()['convertMode']
         num = parser.parse_args()['num']
         if len(content)>0:
             sentence = []
-            for v in HanLP.extractSummary(content, 3):
-                sentence.append(str(v))
+            for v in HanLP.extractSummary(innerConvert(content, '2sc'), 3):
+                sentence.append(innerConvert(str(v), convertMode))
             return {'response': sentence}
         else:
             return {'error': { 'content': '長度不得為零'}}
@@ -269,19 +295,19 @@ class wordDistance(Resource):
     def post(self):
         content = parser.parse_args()['content']
         if len(content)>0:
-            parser.add_argument('comparedTo', type=str, action='append_const')
+            parser.add_argument('comparedTo', type=str, required=True, action='append_const', help='comparedTo無法解析, 存放字串陣列 內含兩個要比對的字串, *必要欄位')
             comparedTo = parser.parse_args()['comparedTo']
 
             CoreSynonymDictionary = JClass('com.hankcs.hanlp.dictionary.CoreSynonymDictionary')
-            distance = CoreSynonymDictionary.distance(comparedTo[0], comparedTo[1])
-            similarity = CoreSynonymDictionary.similarity(comparedTo[0], comparedTo[1])
+
+            distance = CoreSynonymDictionary.distance(innerConvert(comparedTo[0], '2sc'), innerConvert(comparedTo[1], '2sc'))
+            similarity = CoreSynonymDictionary.similarity(innerConvert(comparedTo[0], '2sc'), innerConvert(comparedTo[1], '2sc'))
             return {'response': {
                 'comparedTo': comparedTo,
                 'distance': distance,
                 'similarity': similarity
                 }
             }
-            shutdownJVM()
         else:
             return {'error': { 'content': '長度不得為零'}}
 
@@ -383,9 +409,8 @@ class hk2t(Resource):
             return {'error': { 'content': '長度不得為零'}}
 
 
-api.add_resource(HelloWorld, '/')
 
-#標準分詞
+#分詞工具
 api.add_resource(segment, '/segment')
 api.add_resource(tcSegment, '/tcSegment')
 # api.add_resource(parseDependency, '/parseDependency')
@@ -400,6 +425,9 @@ api.add_resource(nlpTokenizer, '/nlpTokenizer')
 
 #分詞優先分出url
 api.add_resource(urlTokenizer, '/urlTokenizer')
+
+#去除停用詞 todo
+# api.add_resource(notionalTokenizer, '/notionalTokenizer')
 
 #分詞優先分出量詞
 api.add_resource(numberAndQuantifierRecognition, '/quantifier')
@@ -424,7 +452,8 @@ api.add_resource(rewrite, '/rewrite')
 #文章總結
 api.add_resource(summary, '/summary')
 
-api.add_resource(wordDistance, '/wordDistance')
+#比對字串的相似性 todo
+# api.add_resource(wordDistance, '/wordDistance')
 
 
 #文字轉換
