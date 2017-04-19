81abe5bfe95bc7a6f51c47769d831d580f2d15ca
Tid Huang <yesseecity@yahoo.com.tw>
2017-04-19 15:49:48 +0800

    Remove python package termcolor

--- server/modules/apiLogging.py
+++ server/modules/apiLogging.py
@@ -1,7 +1,6 @@
 # coding=UTF-8
 import logging, datetime
 import logging.handlers
-from termcolor import cprint
 
 # logging.basicConfig(filename='/hanlp/log/apiRequest.log', filemode='w', level=logging.DEBUG)
 handler = logging.handlers.RotatingFileHandler('/hanlp/log/apiRequest.log', maxBytes=50*1024*1024, backupCount=5)
@@ -9,17 +8,13 @@ logging.basicConfig( filemode='a', level=logging.DEBUG, handlers=[handler])
 
 def inputMessage(message):
     if len(message):
-        cprint('input: '+ message, 'white')
         logging.info('input: ' + message)
 
 def keyword(message):
-    cprint('keywordList: '+ message, 'blue')
     logging.info('keywordList: ' + message)
 
 def info(message):
-    cprint('info: '+ message, 'blue')
     logging.info(message)
 
 def error(message):
-    cprint('error: '+ message, 'red')
     logging.error(message)
